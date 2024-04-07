8e7b43be3814eef9dc537378a320807cde78d9c1
Ryan Pham <rtpham10@gmail.com>
2024-04-06 22:49:20 -0700

    test

---
+++ test/script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container')
+
+const box = document.createElement('div')
+box.style.width = '50px';
+box.style.height = '50px';
+box.style.backgroundColor = 'red';
+
+container.appendChild(box);
